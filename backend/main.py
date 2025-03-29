def280cb628689769c16458cda05ba3ac18c13b9
David gomez <david.gomez04@hotmail.com>
2025-03-28 21:44:37 -0500

    agrego permisos de cors

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import  HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from config.database import Base, engine
 from routers.user import user_router
 from routers.products import product_router
@@ -11,6 +12,14 @@ app = FastAPI()
 app.title = "e-commerce-api"
 #le damos la version de la api
 app.version = "0.0.1"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos
+    allow_headers=["*"],  # Permitir todas las cabeceras
+)
 
 #configuramos y creamos la bd
 Base.metadata.create_all(bind=engine)
