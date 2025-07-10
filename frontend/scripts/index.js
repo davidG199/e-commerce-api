999c73e6d9a300c8adb3c83491cf7690bd426d97
David gomez <david.gomez04@hotmail.com>
2025-07-09 19:59:24 -0500

    logica para el modal y estilos

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -16,6 +16,7 @@ function getProducts() {
     .catch((error) => console.error("Error al obtener los datos", error));
 }
 
+//creamos las cards dinamicamente
 function cardProduct(producto) {
   const card = document.createElement("div");
   card.classList.add("card_producto");
@@ -26,21 +27,51 @@ function cardProduct(producto) {
   const descripcionProducto = producto.description;
   const precioProducto = producto.price;
   const categoriaProducto = producto.category;
-  const imagenProducto = producto.image_url.startsWith("http") 
-    ? producto.image_url 
+  const imagenProducto = producto.image_url.startsWith("http")
+    ? producto.image_url
     : `http://localhost:8000/${producto.image_url}`;
 
   card.innerHTML = `
-      <img src="${imagenProducto}" alt="${producto.name}";" />
-      <div>
-        <h3>${nombreProducto}</h3>
-        <p>${descripcionProducto}</p>
-        <span>
-          <p>$${precioProducto}</p>
-          <p>${categoriaProducto}</p>
-        </span>
-      </div>
-    `;
+        <img src="${imagenProducto}" alt="${producto.name}";" />
+        <div>
+          <h3>${nombreProducto}</h3>
+          <p>${descripcionProducto}</p>
+          <span>
+            <p>$${precioProducto}</p>
+            <p>${categoriaProducto}</p>
+          </span>
+        </div>
+      `;
+
+  //desde aca abrimos o cerramos el modal
+  card.addEventListener("click", () => {
+    modalCard(producto);
+  });
 
   contenedorProductos.appendChild(card);
 }
+
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("closeModal");
+
+//funcion para crear los modales
+const modalCard = (producto) => {
+  document.getElementById("modalImg").src = producto.image_url;
+  document.getElementById("modalName").textContent = producto.name;
+  document.getElementById("modalDescription").textContent =
+    producto.description;
+  document.getElementById("modalPrice").textContent = producto.price;
+  document.getElementById("modalCategory").textContent = producto.category;
+  document.getElementById("modalStock").textContent = producto.quantity;
+
+  modal.style.display = "flex";
+};
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Cerrar el modal al hacer click afuera de él
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) modal.style.display = "none";
+});
